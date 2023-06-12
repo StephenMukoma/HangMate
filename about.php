fcbcba61b2c728a23d09e0874ed58a9283732666
StephenMukoma <mbuthastevemukoma@gmail.com>
2023-06-12 18:59:30 +0300

    updated features in about page

--- about.php
+++ about.php
@@ -11,6 +11,11 @@
 
     <!--font awesome cdn link-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <!--js breakpoint swiper link-->
+    <link
+      rel="stylesheet"
+      href="https://unpkg.com/swiper/swiper-bundle.min.css"
+    />
 
     <!--custom css file link -->
     <link rel="stylesheet" href="css/style.css">
@@ -37,13 +42,117 @@
 </section>
 <!--header section ends-->
 
-<div class="heading" style="background:url(images/footer-img.jpg) no-repeat">
+<div class="heading" style="background:url(images/aboutcover.jpg) no-repeat">
     <h1>About us</h1>
 </div>
 
+<!--about section starts-->
 
+<section class="about">
 
+    <div class="image">
+        <img src="images/about-img.jpg" alt="">
+    </div>
 
+    <div class="content">
+        <h3>why choose us?</h3>
+        <p>Discover, plan, and create unforgettable moments with friends! We connect you with local activities, plan unforgettable hangouts and capture worthwhile memories.</p>
+        <p>Join us now to explore, connect and make every moment count</p>
+        <div class="icons-container">
+            <div class="icons">
+                <i class="fas fa-map"></i>
+                <span>Top destinations</span>
+            </div>
+            <div class="icons">
+                <i class="fas fa-hand-holding-usd"></i>
+                <span>affordable price</span>
+            </div>
+            <div class="icons">
+                <i class="fas fa-headset"></i>
+                <span>24/7 bookings</span>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<!--about section ends-->
+
+<!--reviews section starts-->
+
+<section class="reviews">
+
+    <div class="swiper reviews-slider">
+
+        <div class="swiper-wrapper">
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>This page is a game changer! It effortlessly plans exciting activities. I love how easy it is to discover local events and create memorable moments. Highly recommended!</p>
+                <h3>Sarah</h3>
+                <img src="images/r-sarah.jpg" alt="">
+            </div>
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>I can't imagine my social life without this hangout app. It has become my go-to platform for discovering new places. A must-have!</p>
+                <h3>Alex</h3>
+                <img src="images/r-2.jpg" alt="">
+            </div>
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>Finally, an app that simplifies hangout planning. It's user friendly and has a wide range of activities to choose from.</p>
+                <h3>Chloe</h3>
+                <img src="images/r-chloe.jpg" alt="">
+            </div>
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>I'm amazed by the seamless experience this hangout app provides. From planning hangouts with friends to exploring unique activities, it has everything covered.</p>
+                <h3>Happy explorer</h3>
+                <img src="images/r-1.jpg" alt="">
+            </div>
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>This hangout app has revolutionized how I spend my free time. It's engaging, and offers a wide variety of events and hangout options.</p>
+                <h3>Adventure Seeker</h3>
+                <img src="images/r-51.jpeg" alt="">
+            </div>
+
+        </div>
+    </div>
+
+</section>
+<!--reviews section ends-->
 
 
 
@@ -113,9 +222,10 @@
 
 
 <!--swiper js link-->
-<!--<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>-->
+<script src="https://codesandbox.io/s/3blv5"></script>
 
 <!--custom js file link-->
 <script src="js/script.js"></script>
+
 </body>
 </html>
